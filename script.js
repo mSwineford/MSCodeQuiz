08b2573f40ff8ca8b7461119e0a2720c44b9ec36
Matthew Swineford <m.swineford@yahoo.com>
2022-09-02 11:52:55 -0400

    fixed timer being in the button. moved timer to upper right corner of screen

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 var startEl = document.querySelector("#start");
+var timerEl = document.getElementById("countdown");
+var mainEl = document.getElementById("main");
+
+var timer = 15;
+var interval = setInterval(function () {
+  document.getElementById("timer").innerHTML = start;
+  start--;
+  if (timer === 0) {
+    clearInterval(interval);
+    document.getElementById("count").innerHTML = "nextQuestion";
+  }
+}, 1000);
 
 startEl.addEventListener("click", function () {});
